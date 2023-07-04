be8f5c9edbac10f360e6294af60f13a65c6e8614
Vladimir Polenok <Vladimir.Polenok.ext@rev-log.com>
2023-07-04 11:59:22 +0200

    Add project skeleton

---
+++ TAF/Core/Configuration/BrowserConfiguration.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Core.Configuration
+{
+    public class BrowserConfiguration : IConfiguration
+    {
+        public string SectionName => "Browser";
+
+        public bool Hedless { get; set; }
+        public string Type { get; set; }
+        public int TimeOut { get; set; }
+    }
+}
